27a7e5083444140132c5ef407236bb217c3f0497
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-05-08 02:00:41 -0600

    feat: Add lexer for basic control flow statements
    The code changes add a lexer for basic control flow statements such as `for`, `if`, `do`, `while`, and `else`. This lexer will be used for parsing and tokenizing code in the API back-end.

--- Api-back-end/server.py
+++ Api-back-end/server.py
@@ -1,10 +1,40 @@
 from flask import Flask
 from dotenv import load_dotenv
 import os
+import ply.lex as lex
 
 load_dotenv()
 
 app = Flask(__name__)
+tokens = (
+    'FOR',
+    'IF',
+    'DO',
+    'WHILE',
+    'ELSE',
+    'LPAREN',
+    'RPAREN',
+)
+
+t_FOR = r'for'
+t_IF = r'if'
+t_DO = r'do'
+t_WHILE = r'while'
+t_ELSE = r'else'
+t_LPAREN = r'\('
+t_RPAREN = r'\)'
+
+t_ignore  = ' \t'
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print(f"Illegal character '{t.value[0]}'")
+    t.lexer.skip(1)
+
+lexer = lex.lex()
 
 @app.route('/')
 def hello_world():
