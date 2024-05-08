7e6f29090ce3bc642c62abff10250b9ea9f34f90
NoeAlejandroRodriguezMoto <alxg5516@gmail.com>
2024-05-08 04:46:37 -0600

    chore: Actualizar estilos y diseño del formulario en el front-end

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -17,18 +17,26 @@ function App() {
 
   const onFileFormSubmit = async (event) => {
     event.preventDefault();
+    if (!file) {
+      alert('Por favor, selecciona un archivo');
+      return;
+    }
     const formData = new FormData();
     formData.append('file', file);
     const response = await fetch('http://localhost:3003/upload', {
       method: 'POST',
       body: formData,
     });
-    const data = await response.json(); // Parse JSON response
+    const data = await response.json();
     setResponse(data);
   };
 
   const onCodeFormSubmit = async (event) => {
     event.preventDefault();
+    if (!code.trim()) {
+      alert('Por favor, introduce código');
+      return;
+    }
     const responseOnCode = await fetch('http://localhost:3003/analyze', {
       method: 'POST',
       body: code,
@@ -36,7 +44,7 @@ function App() {
         'Content-Type': 'text/plain',
       },
     });
-    const data = await responseOnCode.json(); // Parse JSON response
+    const data = await responseOnCode.json();
     setResponseOnCode(data);
   };
 
@@ -44,7 +52,21 @@ function App() {
     <div className='container'>
       <div className='container-A'>
         <form className='form-A' onSubmit={onFileFormSubmit}>
-          <input id='file' type="file" onChange={onFileChange} />
+          <label className="custum-file-upload" for="file">
+            <div className="icon">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24">
+                <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                <g stroke-linejoin="round" stroke-linecap="round" id="SVGRepo_tracerCarrier"></g>
+                <g id="SVGRepo_iconCarrier">
+                  <path fill="" d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z" clip-rule="evenodd" fill-rule="evenodd"></path>
+                </g>
+              </svg>
+            </div>
+            <div className="text">
+              <span>Click to upload File</span>
+            </div>
+            <input className='upload-file' id='file' type="file" onChange={onFileChange} />
+          </label>
           <button type="submit">Subir y Analizar</button>
         </form>
         {response && (
@@ -52,6 +74,7 @@ function App() {
             <thead>
               <tr>
                 <th>Línea</th>
+                <th>Reservada</th>
                 <th>Símbolo</th>
               </tr>
             </thead>
@@ -59,7 +82,8 @@ function App() {
               {response.map((item, index) => (
                 <tr key={index}>
                   <td>{item.linea}</td>
-                  <td>{item.simbolo}</td>
+                  <td>{item.reserved}</td>
+                  <td>{item.symbol}</td>
                 </tr>
               ))}
             </tbody>
@@ -68,7 +92,7 @@ function App() {
       </div>
       <div className='container-B'>
         <form className='form-B' onSubmit={onCodeFormSubmit}>
-          <textarea className='text-area-B' onChange={onCodeChange} value={code} />
+          <textarea placeholder='Introduce el codigo que deseas analizar' className='text-area-B' onChange={onCodeChange} value={code} />
           <button type="submit">Analizar Código</button>
         </form>
         {responseOnCode && (
@@ -76,6 +100,7 @@ function App() {
             <thead>
               <tr>
                 <th>Línea</th>
+                <th>Reservada</th>
                 <th>Símbolo</th>
               </tr>
             </thead>
@@ -83,7 +108,8 @@ function App() {
               {responseOnCode.map((item, index) => (
                 <tr key={index}>
                   <td>{item.linea}</td>
-                  <td>{item.simbolo}</td>
+                  <td>{item.reserved}</td>
+                  <td>{item.symbol}</td>
                 </tr>
               ))}
             </tbody>
